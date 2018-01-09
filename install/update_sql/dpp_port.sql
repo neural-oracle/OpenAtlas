cf1e8f4d43fbd620f71540c13638b27906f0589f
Alexander Watzinger <alexander.watzinger@craws.net>
2018-01-09 05:20:48 +0100

    Removed DPP specific update sql

--- install/update_sql/dpp_port.sql
+++
@@ -1,6 +0,0 @@
-
--- delete orphaned data which breaks application
-DELETE FROM gis.polygon WHERE id = 134;
-
--- delete BC dates until issue is solved
-DELETE FROM model.entity WHERE value_timestamp < '0001-01-01';
