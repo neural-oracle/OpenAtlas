b550767e89bbcab0f7a2cb056020c4752bdba798
Alexander Watzinger <alexander.watzinger@craws.net>
2018-01-09 05:19:37 +0100

    Removed MEDCON specific update sql

--- install/update_sql/medcon_port.sql
+++
@@ -1,5 +0,0 @@
--- delete orphaned data which breaks application
-DELETE FROM model.entity WHERE id in (1043);
-
--- delete BC dates until issue is solved
-DELETE FROM model.entity WHERE value_timestamp < '0001-01-01';
