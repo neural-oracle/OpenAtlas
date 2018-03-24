c5feac33ec5c2fb75ec6d656f36ea8e8f7e9c147
Alexander Watzinger <alexander.watzinger@craws.net>
2018-03-24 15:56:09 +0100

    Ostalpen import: removed hardcoded types, update reset sql

--- install/ostalpen/import.py
+++ install/ostalpen/import.py
@@ -1,50 +1,50 @@
+import os
 import subprocess
+import sys
 import time
 
 import psycopg2.extras
 
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+
 """
 To do:
 
-- add 3.3.0 update sql to orig db
-- remove hardcoded ids for types eg: 37 with type id from "Original text"
 - add type case study - eastern alps
 - add source links to actors, places, ... (add documented in)
-
 
 After gis implementation:
 - split case studies
 
-At the end:
-- compare links created with links existing
 """
 
-start = time.time()
-db_pass = open('instance/password.txt').read().splitlines()[0]
 
-ostalpen_user_id = 38
-ostalpen_type_id = 11821
-
-# make a fresh database
-subprocess.call('dropdb openatlas_dpp', shell=True)
-subprocess.call('createdb openatlas_dpp -O openatlas', shell=True)
-subprocess.call('psql openatlas_dpp < instance/dpp_origin.sql', shell=True)
+class Entity:
+    system_type = None
 
 
 def connect(database_name):
+    db_pass = open('instance/password.txt').read().splitlines()[0]
     connection = psycopg2.connect(database=database_name, user='openatlas', password=db_pass)
     connection.autocommit = True
     return connection
 
 
+def reset_database():
+    subprocess.call('dropdb openatlas_dpp', shell=True)
+    subprocess.call('createdb openatlas_dpp -O openatlas', shell=True)
+    subprocess.call('psql openatlas_dpp < instance/dpp_origin.sql', shell=True)
+
+
+start = time.time()
+ostalpen_user_id = 38
+ostalpen_type_id = 11821
+reset_database()
 connection_dpp = connect('openatlas_dpp')
 connection_ostalpen = connect('ostalpen')
 cursor_dpp = connection_dpp.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
 cursor_ostalpen = connection_ostalpen.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
-
-
-class Entity:
-    system_type = None
 
 
 def link(property_code, domain_id, range_id, description=None):
@@ -97,13 +97,13 @@ def insert_entity(entity):
     cursor_dpp.execute(sql, {'entity_id': entity.id, 'created': entity.created})
 
 
+# Add comment to ostalpen_id
 sql_ = """
     ALTER TABLE model.entity ADD COLUMN ostalpen_id integer;
     COMMENT ON COLUMN model.entity.ostalpen_id IS 'uid of former Ostalpen table tbl_entities';"""
-
 cursor_dpp.execute(sql_)
 
-entities = []
+# Set counters
 new_entities = {}
 missing_classes = {}
 count = {
@@ -114,6 +114,16 @@ count = {
     'E74 group': 0,
     'E8 acquisition': 0,
     'link': 0}
+
+# Get DPP types
+types = {}
+cursor_dpp.execute("SELECT id, name FROM model.entity WHERE class_code = 'E55';")
+for row in cursor_dpp.fetchall():
+    if row.name in types:
+        print('Warning - double type entry for: ' + row.name)
+    types[row.name] = row.id
+
+# Get ostalpen entities
 sql_ = """
     SELECT
         uid, entity_name_uri, cidoc_class_nr, entity_type, entity_description, start_time_abs,
@@ -121,7 +131,7 @@ sql_ = """
     FROM openatlas.tbl_entities e
     JOIN openatlas.tbl_classes c ON e.classes_uid = c.tbl_classes_uid;"""
 cursor_ostalpen.execute(sql_)
-
+entities = []
 for row in cursor_ostalpen.fetchall():
     e = Entity()
     e.created = row.timestamp_creation
@@ -134,6 +144,7 @@ for row in cursor_ostalpen.fetchall():
     e.start_time_abs = row.start_time_abs
     entities.append(e)
 
+# Insert entities in DPP
 for e in entities:
     if not e.name:
         continue
@@ -153,7 +164,6 @@ for e in entities:
             count['E33 source'] += 1
         e.class_code = 'E33'
         insert_entity(e)
-
     elif e.class_code == 'E074':
         e.class_code = 'E74'
         insert_entity(e)
@@ -167,25 +177,24 @@ for e in entities:
         continue
     new_entities[e.ostalpen_id] = e
 
+# Get links in DPP
 sql_ = """
     SELECT links_uid, links_entity_uid_from, links_cidoc_number_direction, links_entity_uid_to,
         links_annotation, links_creator, links_timestamp_start, links_timestamp_end,
         links_timestamp_creation, links_timespan
     FROM openatlas.tbl_links;"""
-
 cursor_ostalpen.execute(sql_)
-
 for row in cursor_ostalpen.fetchall():
     if row.links_cidoc_number_direction == 33:  # has translation
         domain = new_entities[row.links_entity_uid_from]
         range_ = new_entities[row.links_entity_uid_to]
         if domain.id_name.startswith('tbl_2_quelle_original'):
             link('P73', range_.id, domain.id, row.links_annotation)
-            link('P2', domain.id, 37)
+            link('P2', domain.id, types['Original text'])
             count['link'] += 2
         elif domain.id_name.startswith('tbl_2_quelle_uebersetzung'):
             link('P73', range_.id, domain.id, row.links_annotation)
-            link('P2', domain.id, 38)
+            link('P2', domain.id, types['Translation'])
             count['link'] += 2
         else:
             print('Error missing translation type, id: ' + str(domain.id) + ', ' + domain.id_name)
