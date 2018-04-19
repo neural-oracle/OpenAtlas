b529707de147b4f4057a2166cb121b782f8a2dc1
Alexander Watzinger <alexander.watzinger@craws.net>
2018-04-19 17:57:21 +0200

    Ostalpen place import script with Stefan

--- install/ostalpen/import.py
+++ install/ostalpen/import.py
@@ -11,7 +11,13 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 """
 To do:
 
-- next persons?
+Places:
+ - geom
+ - links to sources
+ - other links
+ - feature, su, ...
+ - dates,
+ - types
 
 After gis implementation:
 - split case studies
@@ -64,7 +70,7 @@ def link(property_code, domain_id, range_id, description=None):
     return cursor_dpp.fetchone()[0]
 
 
-def insert_entity(entity, with_case=False):
+def insert_entity(entity, with_case_study=False):
     sql = """
         INSERT INTO model.entity (name, description, class_code, system_type, ostalpen_id, created)
         VALUES (%(name)s, %(description)s, %(class_code)s, %(system_type)s, %(ostalpen_id)s,
@@ -95,12 +101,13 @@ def insert_entity(entity, with_case=False):
         VALUES (%(ostalpen_user_id)s, 'insert', %(entity_id)s, %(created)s);"""
     cursor_dpp.execute(sql, {
         'ostalpen_user_id': ostalpen_user_id, 'entity_id': entity.id, 'created': entity.created})
-    if with_case:
+    if with_case_study:
         sql = """
             INSERT INTO model.link (property_code, domain_id, range_id)
             VALUES (%(property_code)s, %(domain_id)s, %(range_id)s);"""
         cursor_dpp.execute(sql, {
             'property_code': 'P2', 'domain_id': entity.id, 'range_id': ostalpen_type_id})
+    return entity.id
 
 
 # Add comment to ostalpen_id
@@ -114,6 +121,7 @@ new_entities = {}
 missing_classes = {}
 count = {
     'E21 person': 0,
+    'E18 place': 0,
     'E33 source': 0,
     'E33 translation': 0,
     'E33 original text': 0,
@@ -156,7 +164,7 @@ for e in entities:
         continue
     if e.class_code == 'E021':  # Person
         e.class_code = 'E21'
-        insert_entity(e, with_case=True)
+        insert_entity(e, with_case_study=True)
         count['E21 person'] += 1
     elif e.class_code == 'E033':  # Linguistic Object (Source)
         if e.id_name.startswith('tbl_2_quelle_original'):
@@ -169,19 +177,68 @@ for e in entities:
             e.system_type = 'source content'
             count['E33 source'] += 1
         e.class_code = 'E33'
-        insert_entity(e, with_case=True if e.system_type == 'source content' else False)
+        insert_entity(e, with_case_study=True if e.system_type == 'source content' else False)
     elif e.class_code == 'E074':  # Group
         e.class_code = 'E74'
-        insert_entity(e, with_case=True)
+        insert_entity(e, with_case_study=True)
         count['E74 group'] += 1
     elif e.class_code == 'E008':  # Acquisition
         e.class_code = 'E8'
-        insert_entity(e, with_case=True)
+        insert_entity(e, with_case_study=True)
         count['E8 acquisition'] += 1
+    elif e.class_code in ['E18', 'E53']:
+        continue # place will be added later
     else:
         missing_classes[e.class_code] = e.class_code
         continue
     new_entities[e.ostalpen_id] = e
+
+# Insert places
+
+sql_ = """
+    SELECT
+        uid, entity_name_uri, entity_type, entity_description, start_time_abs,
+        end_time_abs, start_time_text, end_time_text, timestamp_creation, name_path
+    FROM openatlas.sites;"""
+cursor_ostalpen.execute(sql_)
+places = []
+for row in cursor_ostalpen.fetchall():
+    e = Entity()
+    e.created = row.timestamp_creation
+    e.ostalpen_id = row.uid
+    e.name = row.entity_name_uri
+    e.description = row.entity_description
+    e.start_time_text = row.start_time_text
+    e.start_time_abs = row.start_time_abs
+    places.append(e)
+
+for e in places:
+    if not e.name:
+        continue
+    # split place, features ...
+    e.class_code = 'E18'
+    e.system_type = 'place'
+    object_id = insert_entity(e, with_case_study=True)
+    e.system_type = 'place location'
+    e.class_code = 'E53'
+    e.name = 'Location of ' + e.name
+    location_id = insert_entity(e)
+    link('P53', object_id, location_id)
+
+
+    """
+    SELECT ST_AsText(
+    
+      ST_Transform(
+        ST_GeomFromText('POINT(646274.7681 5429433.0725)',32633),4326)
+      
+    
+    ) AS whatever FROM openatlas.sites WHERE srid_epsg = 32633;"""
+
+
+
+    count['E18 place'] += 1
+
 
 # Get links in DPP
 sql_ = """
